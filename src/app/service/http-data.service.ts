eba73ce7027b1a03fc5e8be58cf6b69f75767e83
Bishko86 <polroldon@gmail.com>
2021-08-05 20:47:31 +0300

    implemented http module

---
+++ src/app/service/http-data.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, delay, map } from 'rxjs/operators';
+export interface JsonDataType {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: {
+      lat: string
+      lng: string
+    }
+  }
+  phone: string
+  website: string
+  company: {
+    name: string
+    catchPhrase: string
+    bs: string
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpDataService {
+  constructor(private http: HttpClient) { }
+  private limit = 5
+  private portion = 5
+  private localeData: JsonDataType[] = []
+  private composeData(data: JsonDataType[]) {
+    data.forEach(user => {
+      if (this.localeData.length < 10) {
+        this.localeData.push(user)
+      }
+      else {
+        this.localeData.push({
+          ...user,
+          id: this.localeData[this.localeData.length - 1].id
+            + 1
+        })
+      }
+    })
+  }
+  fetchData() {
+    this.portion = this.portion === 5 ? 0 : 5
+    return this.http.get<JsonDataType[]>(`https://jsonplaceholder.typicode.com/users?_start=${this.portion}&_limit=${this.limit}`)
+      .pipe(map(
+        (data: JsonDataType[]) => { this.composeData(data) }),
+        catchError((error) => throwError(error)
+        ));
+  }
+  getById(id: number) {
+    return this.localeData.find(item => item.id === id)
+  }
+  getUsers(){
+    return this.localeData;
+  }
+}
