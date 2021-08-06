44110e566e12968d0808b75bc11f94f8715d355b
Bishko86 <polroldon@gmail.com>
2021-08-07 00:00:37 +0300

    edit rotes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CardComponent } from './card/card.component';
-import { FeaturesComponent } from './features/features.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'features', component: FeaturesComponent },
-  { path: 'features/:id/:username', component: CardComponent }
+  {path:'features',
+    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
+    }
 ];
 
 @NgModule({
   imports: [
-    // RouterModule.forRoot(routes),
     RouterModule.forRoot(routes, {
       scrollPositionRestoration: 'enabled', // Add options right here
     }),
