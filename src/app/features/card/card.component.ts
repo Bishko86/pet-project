2a713d7856cc2ec0482cce07477bb47b2266acab
Bishko86 <polroldon@gmail.com>
2021-08-06 23:54:22 +0300

    move folder to new location

---
+++ src/app/features/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpDataService, JsonDataType } from '../../service/http-data.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  item: JsonDataType | undefined
+  constructor(private route: ActivatedRoute,
+    private httpService: HttpDataService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.item = this.httpService.getById(+params.id)
+    })
+  }
+
+
+
+}
